b24435d92fadf755389b555624008955af5d28bb
AbielRC97 <109878636+AbielRC97@users.noreply.github.com>
2025-05-22 23:11:45 -0600

    Validaciones de campos

--- BlazorCRUD.Shared/PersonDTO.cs
+++ BlazorCRUD.Shared/PersonDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace BlazorCRUD.Shared
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorCRUD.Shared
 {
     public class PersonDTO
     {
@@ -10,10 +12,24 @@
 
     public class RegisterDTO
     {
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100, ErrorMessage = "El nombre debe tener máximo 100 caracteres")]
         public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido paterno es obligatorio")]
+        [StringLength(100, ErrorMessage = "El apellido paterno debe tener máximo 100 caracteres")]
         public string ApellidoPaterno { get; set; }
+
+        [Required(ErrorMessage = "El apellido materno es obligatorio")]
+        [StringLength(100, ErrorMessage = "El apellido materno debe tener máximo 100 caracteres")]
         public string ApellidoMaterno { get; set; }
+
+        [Required(ErrorMessage = "La fecha de nacimiento es obligatoria")]
+        [DataType(DataType.Date)]
         public DateTime FechaNacimiento { get; set; }
+
+        [Required(ErrorMessage = "El sexo es obligatorio")]
+        [RegularExpression("[MF]", ErrorMessage = "Sexo debe ser 'M' o 'F'")]
         public char Sexo { get; set; }
     }
 
