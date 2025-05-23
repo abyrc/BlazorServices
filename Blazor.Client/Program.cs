22fe2cc436971606357c7b01ce65bb86e9e5eac5
AbielRC97 <109878636+AbielRC97@users.noreply.github.com>
2025-05-23 16:27:50 -0600

    ngrok

--- Blazor.Client/Program.cs
+++ Blazor.Client/Program.cs
@@ -1,15 +1,23 @@
 using Blazor.Client;
 using Blazor.Client.Interfaces;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazor.Client.Services;
 using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Net.Http.Json;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000") });
+// Cargar configuración desde appsettings.json
+using var http = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+var config = await http.GetFromJsonAsync<Dictionary<string, string>>("appsettings.json");
+
+string baseUrl = config["BaseUrl"];
+Console.WriteLine($"Base URL: {baseUrl}");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseUrl) });
 
 builder.Services.AddScoped<IPersonService, PersonaService>();
 
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 ```bash
-docker-compose up --build
+docker compose down
+docker compose up --build
 dotnet ef database update --project Blazor.Server/Blazor.Server.csproj
 dotnet ef migrations script --output migration.sql --project Blazor.Server/Blazor.Server.csproj
 ```
@@ -9,4 +10,13 @@ sqp_12f14696be1cdead65f18a5b50e9834aa762cc7f
 dotnet sonarscanner begin /k:"blazor-app" /d:sonar.host.url="http://localhost:9000"  /d:sonar.token="sqp_12f14696be1cdead65f18a5b50e9834aa762cc7f"
 dotnet build
 dotnet sonarscanner end /d:sonar.token="sqp_12f14696be1cdead65f18a5b50e9834aa762cc7f"
+```
+
+```bash
+2xVqc9THqz1eIYZdTdSra9NGnEh_7wPkL3c4b8pp1TzKaR99T
+ngrok http http://localhost:8080
+ngrok http http://localhost:5000
+ngrok config add-authtoken 2xVqc9THqz1eIYZdTdSra9NGnEh_7wPkL3c4b8pp1TzKaR99T
+ngrok start --config ./ngrok.yml --all
+
 ```
